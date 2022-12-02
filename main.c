e640864de867a382ffbcd7089c8a6b8f5881aeff
Saksham Attri <sakshamattri3@gmail.com>
2022-12-02 13:43:17 +0530

    Verify AND introduction

--- main.c
+++ main.c
@@ -1,23 +1,251 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
-int and_intro(){}
+int top = -1;
+
+// checking if the stack is full
+/** @brief Definition of isFull() function
+ * @return Whether or not the stack is full
+ */
+int isFull(int lenInfix) {
+    if(top == lenInfix - 1) return 1;
+    else return 0;
+} 
+
+// checking if the stack is empty
+/** @brief Definition of isEmpty() function
+ * @return Whether or not the stack is empty
+ */
+int isEmpty() {
+    if(top == -1) return 1;
+    else return 0; 
+}
+
+// push function which inserts value in the stack and increments top
+/** @brief Definition of push() function
+ * @return Void
+ */
+void push(char item,int lenInfix,char * stack) {
+    if (isFull(lenInfix)) return; 
+	top++;
+	stack[top] = item;
+}
+
+// pop function which removes an item from the stack and decrements top 
+/** @brief Definition of pop() function
+ * @return Pops the uppermost element of the stack if it is not empty
+ */
+int pop(char * stack) { 
+    if (isEmpty()) return INT_MIN; 
+        
+    // decrements top and returns what has been popped      
+    return stack[top--]; 
+} 
+
+/** @brief Definition of peek() function
+ * @return Returns the topmost element of the stack if it is not empty
+ */
+int peek(char * stack){ 
+    if (isEmpty()) return INT_MIN; 
+    return stack[top];
+} 
+
+/** @brief Definition of checkIfOperand() function
+ * @return Whether or not the character is a propositional atom
+ */
+// a function to check if the given character is a propositional atom 
+int checkIfOperand(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+} 
+
+/** @brief Definition of priority() function
+ * @return Returns a priority value depending on the operator.
+ */
+// priority function to compare priority. It returns a larger value for a higher priority operator   
+int priority(char ch) 
+{ 
+    switch(ch) {
+        case '~':        
+		return 5;
+		break;
+
+        case '*':    
+		return 4;
+		break;
+
+		case '+':
+		return 3;
+		break;
+
+        case '>':
+        return 2;
+        break;
+
+		case '(':       
+		case ')':
+		return 1;
+		break;
+	}
+    return -1; 
+}
+
+/** @brief Definition of getPostfix() function
+ * 
+ */
+// getPostfix function to convert an infix expression to a postfix expression
+int getPostfix(char* expression, char * stack, int lenInfix) 
+{ 
+    int i, j;
+
+    for (i = 0, j = -1; expression[i]; ++i) 
+    { 
+        // we check whether the scanned character is an operand or not and add this to the output
+        if (checkIfOperand(expression[i])) expression[++j] = expression[i]; 
+
+        // we push '(' to the stack if we scan it
+        else if (expression[i] == '(') push(expression[i],lenInfix,stack); 
+
+        // if the scanned character is ')', we need to pop from the stack till '(' is encountered in the stack 
+        else if (expression[i] == ')') 
+        { 
+            while (!isEmpty(stack) && peek(stack) != '(') expression[++j] = pop(stack);
+            
+            if (!isEmpty(stack) && peek(stack) != '(')return -1; // invalid expression              
+            else pop(stack); 
+        } 
+        else // if the scanned character is an operator
+        { 
+            while (!isEmpty(stack) && priority(expression[i]) <= priority(peek(stack))) expression[++j] = pop(stack); 
+            push(expression[i],lenInfix,stack); 
+        } 
+
+    } 
+
+    // Once all inital expression characters are traversed, add all remaining elements to exp from stack
+    while (!isEmpty(stack)) expression[++j] = pop(stack); 
+
+    expression[++j] = '\0'; 
+    
+}
+
+/** @brief Definition of reverse() function
+ * @return Void
+ */
+// a function to reverse a string
+void reverse(char *exp){
+
+    int size = strlen(exp);
+    int j = size, i=0;
+    char temp[size];
+
+    temp[j--]='\0';
+    while(exp[i]!='\0')
+    {
+        temp[j] = exp[i];
+        j--;
+        i++;
+    }
+    strcpy(exp,temp);
+}
+
+/** @brief Definition of brackets() function
+ * @return Void
+ */
+// a function to swap brackets
+void brackets(char* exp){
+    int i = 0;
+    while(exp[i]!='\0')
+    {
+        if(exp[i]=='(')
+            exp[i]=')';
+        else if(exp[i]==')')
+            exp[i]='(';
+        i++;
+    }
+}
+
+// the function to convert the infix expression into a prefix expression
+
+/** @brief Definition of infixToPrefix() function
+ *  @return The length of the infix expression
+ */
+int infixToPrefix(char *exp, char * stack, char * prefix, int lenInfix)
+{
+
+    int size = strlen(exp);
+
+    // reverse string
+    reverse(exp);
+    // swap brackets
+    brackets(exp);
+    // get the postfix expression 
+    getPostfix(exp,stack,lenInfix);
+    // reverse the string again to get the prefix expression
+    reverse(exp);
+    strcpy(prefix,exp);
+    return strlen(exp);
+}
+
+int and_intro(int line, int l1, int l2, char prefix1[], char prefix2[], char * buffer2[]){
+    if (l1 >= line || l2 >= line)
+        return 0;
+    if (strcmp(prefix1,buffer2[l1])==0){
+        if(strcmp(prefix2,buffer2[l2])==0){
+            return 1;
+        }
+    }
+    return 0;
+}
 int and_elim(){}
 int or_intro(){}
 int imp_elim(){}
 
 int ln;
 
-int checkline(int i, char * buffer[]){
-  int index = 0;
+int checkline(int i, char * buffer[], char * buffer2[]){
+  int index = 0, index2 = 0, temp = 0, min = 10;
   while(buffer[i][index]!='/'){
     index++;
+    if(buffer[i][index]=='(')
+      temp++;
+    else if (buffer[i][index]==')')
+      temp--;
+    if (index!=0 && buffer[i][index+1]!='/' && (buffer[i][index]=='+' || buffer[i][index]=='*' || buffer[i][index]=='>')){
+      if (temp<min){
+        min=temp;
+        if (buffer[i][index-1]=='(')
+            index2=index+1;
+        else
+            index2=index;
+      }
+    }
   }
   index++;
+
+  // printf("\n%c at index %d\n",buffer[i][index2],index2);
+  
+
   if (buffer[i][index]=='P')
     return 1;
-  else if (buffer[i][index]=='*' && buffer[i][index+1]=='i')
-    return and_intro();
+
+  char infix1[index2+1], prefix1[index2+1], stack1[index2+1];
+  for (int t = 0; t < index2; t++){
+      infix1[t]=buffer[i][t];
+  }
+  infix1[index2]='\0';
+
+  char infix2[index-index2-1], prefix2[index-index2-1], stack2[index-index2-1];
+  for (int t = 0; t < index-index2-2; t++){
+      infix2[t]=buffer[i][index2+t+1];
+  }
+  infix2[index-index2-2]='\0';
+  int len1 = infixToPrefix(infix1,stack1,prefix1,index2);
+  int len2 = infixToPrefix(infix2,stack2,prefix2,index-2-index2);
+  int r;
+  if (buffer[i][index]=='*' && buffer[i][index+1]=='i')
+    r = and_intro(i,(int) buffer[i][index+3] -49,(int) buffer[i][index+5] -49, prefix1, prefix2, buffer2);
   else if (buffer[i][index]=='*' && buffer[i][index+1]=='e')
     return and_elim();
   else if (buffer[i][index]=='+' && buffer[i][index+1]=='i')
@@ -25,12 +253,13 @@ int checkline(int i, char * buffer[]){
   else if (buffer[i][index]=='>' && buffer[i][index+1]=='e')
     return imp_elim();
   else
-    return 3;
+    r = 3;
+  return r;
 }
 
-int check(char * buffer[],int n){
+int check(char * buffer[],int n, char * buffer2[]){
   for (ln = 0; ln < n; ln++){
-    int eval = checkline(ln,buffer);
+    int eval = checkline(ln,buffer,buffer2);
     if (eval == 0)
       return 0;
     else if (eval == 3)
@@ -74,7 +303,23 @@ int main(){
       buffer[line] = temp;
   }
 
-  int result = check(buffer,n);
+  char * buffer2[n];
+  for (int line = 0; line < n; line++){
+      int inx = 0;
+      while (buffer[line][inx]!='/'){
+          inx++;
+      }
+      char inf[inx+1], stk[inx+1];
+      char * prf = (char *) malloc((inx+1)*sizeof(char));
+      for (int h = 0; h < inx; h++){
+          inf[h]=buffer[line][h];
+      }
+      inf[inx]='\0';
+      int lex = infixToPrefix(inf,stk,prf,inx+1);
+      buffer2[line]=prf;
+  }
+
+  int result = check(buffer,n,buffer2);
   if (result == 0)
     printf("Invalid Proof\n");
   else if (result == 1)
